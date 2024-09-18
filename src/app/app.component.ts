73f82d69ad7a0dc73d9d314f1a3e5c86691e82c9
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-09-18 14:55:34 +0200

    Update structure to upgrade appearance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PeriodicTableComponent } from "./periodic-table/periodic-table.component";
+import { PeriodicTableComponent } from './periodic-table/periodic-table.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PeriodicTableComponent],
+  imports: [RouterOutlet, PeriodicTableComponent, MatCardModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'periodic-tabele';
